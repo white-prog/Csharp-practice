1f0f8fcc08d59464328ee08e6b5d9c634b1fa019
white-prog <sabinhameed55@gmail.com>
2025-01-27 23:27:42 +0530

    LINQ operation by selecting and ordering from a book collection

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+/*
 void SumOfdigits(int value)
 {
     int sum = 0;
@@ -34,4 +34,34 @@ while (true)
     SumOfdigits(number);
 
 }
+*/
 
+public static class Program
+{
+    public static void Main()
+    {
+        List<Book> books = new List<Book>();
+        books.Add(new Book { Title = "The Great Gatsby",Pages = 180,Price = 11});
+        books.Add(new Book { Title = "War and Peace", Pages = 1225, Price = 20 });
+        books.Add(new Book { Title = "Mockingbird", Pages = 324, Price = 8});
+        books.Add(new Book { Title = "1984", Pages =  428, Price = 13 });
+
+        Console.Write("Enter pages: ");
+        int pages = Convert.ToInt32(Console.ReadLine());
+        var book_filter = books.Where(book => book.Pages >= pages).OrderBy(book  => book.Price);
+        foreach (var item in book_filter)
+        {
+            Console.WriteLine($"{item.Title} : {item.Price}");
+            
+        }
+
+    }
+   
+}
+class Book
+{
+    public string Title {  get; set; }
+    public int Pages { get; set; }
+    public int Price { get; set; }
+
+}
